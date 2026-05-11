9360a9de2732c188d944a55c9b4e4218c82af8a3
Alonso Chavarria <alons182@hotmail.com>
2026-05-11 15:59:25 -0600

    Add index.js and Modulo.js with age validation logic

---
+++ src/Modulo.js
@@ -0,0 +1,10 @@
+export const EDAD = 18;
+
+export function esMayorDeEdad(edad) {
+    return edad >= EDAD;
+}
+
+function saludo(nombre) {
+    return `Hola, ${nombre}!`;
+}
+
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { EDAD, esMayorDeEdad } from "./Modulo.js";
+
+console.log(`La edad es: ${EDAD}`);
+console.log(`¿Es mayor de edad? ${esMayorDeEdad(20)}`);
